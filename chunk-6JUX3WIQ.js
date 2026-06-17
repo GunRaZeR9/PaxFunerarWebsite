a01825eff59fc712c2a404b31d60da19045dfaa5
GunRaZeR9 <GunRaZeR9@users.noreply.github.com>
2026-06-17 13:36:26 +0000

    deploy: 46971defd388fc21f9378f834858722cc97e5312

---
+++ chunk-6JUX3WIQ.js
@@ -0,0 +1,1 @@
+var e=[{id:"all",slug:"",labelKey:"shop.cat.all"},{id:"coroane",slug:"coroane",labelKey:"shop.cat.coroane"},{id:"jerbe",slug:"jerbe",labelKey:"shop.cat.jerbe"},{id:"aranjamente",slug:"aranjamente",labelKey:"shop.cat.aranjamente"},{id:"buchete",slug:"buchete",labelKey:"shop.cat.buchete"}];export{e as a};
